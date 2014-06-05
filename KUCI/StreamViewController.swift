a6f74a0238c29c916a690132bcee3fe81308b8b2
Nealon Young <hi@nealyoung.me>
2014-06-05 00:10:32 -0700

    Added streaming functionality, various improvements to storyboard

--- KUCI/StreamViewController.swift
+++ KUCI/StreamViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Nealon Young. All rights reserved.
 //
 
+import AVFoundation
+import MediaPlayer
 import UIKit
 
 class StreamViewController : UIViewController {
@@ -13,4 +15,51 @@ class StreamViewController : UIViewController {
     @IBOutlet var playButton: UIButton
     @IBOutlet var titleLabel: UILabel
     @IBOutlet var hostLabel: UILabel
+    
+    var playing = false
+    var player = MPMoviePlayerController(contentURL: NSURL(string: "http://streamer.kuci.org:8000/high"))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+        var sessionError = NSError()
+        
+        var error : NSError?
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: &error)
+        AVAudioSession.sharedInstance().setActive(true, error: &error)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "toggleStream:", name: "toggleStream", object: nil)
+        
+        self.updateCurrentShow()
+        NSTimer.scheduledTimerWithTimeInterval(30.0, target: self, selector: "updateCurrentShow", userInfo: nil, repeats: true)
+    }
+    
+    func updateCurrentShow() {
+        ScheduleParser.currentShowWithCompletion { (show: Show?) in
+            if show {
+                self.titleLabel.text = show!.title
+                self.hostLabel.text = show!.host
+            } else {
+                self.titleLabel.text = "No program info"
+                self.hostLabel.text = ""
+            }
+        }
+    }
+    
+    @IBAction func playButtonPressed() {
+        toggleStream(nil)
+    }
+    
+    func toggleStream(notification: NSNotification?) {
+        if playing {
+            player.pause()
+            playButton.setBackgroundImage(UIImage(named: "Play"), forState: .Normal)
+        } else {
+            player.play()
+            playButton.setBackgroundImage(UIImage(named: "Pause"), forState: .Normal)
+        }
+        
+        playing = !playing
+    }
 }
